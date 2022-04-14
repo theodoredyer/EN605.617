707e9c7f469aeba777c6e8c8de817c9c22081664
theodoredyer <tdyer@ucsc.edu>
2022-04-13 21:07:21 -0700

    Update thrust_asg.cpp

--- module9/thrust_examples/thrust_asg.cpp
+++ module9/thrust_examples/thrust_asg.cpp
@@ -23,36 +23,68 @@ Chance Pascale
 int main(void) {
 
     // 3 integer long vector
-    thrust::host_vector<int> H(3);
+    thrust::host_vector<int> HA(3);
+    thrust::host_vector<int> HB(3);
 
     // Init vector A elements
-    HA[0] = 2;
-    HA[1] = 4;
-    HA[2] = 8;
+    HA[0] = 8;
+    HA[1] = 12;
+    HA[2] = 16;
 
     // Init vector B elements
-    HB[0] = 8;
-    HB[1] = 12;
-    HB[2] = 16;
+    HB[0] = 2;
+    HB[1] = 4;
+    HB[2] = 6;
 
 
     // Print contents of input vectors
-    for(int i = 0; i < H.size(); i++) {
+    printf("Input vectors: \n");
+    for(int i = 0; i < HA.size(); i++) {
         std::cout << "HA[" << i << "] = " << HA[i] << std::endl;
+    }
+    printf("\n");
+    for(int i = 0; i < HA.size(); i++) {
         std::cout << "HB[" << i << "] = " << HB[i] << std::endl;
     }
 
+    // Set up device vectors and copy data from host
     thrust::device_vector<int> DA = HA;
     thrust::device_vector<int> DB = HB;
-    thrust::device_vector<int> DC = HB;
+    thrust::device_vector<int> DADD = HB;
+    thrust::device_vector<int> DSUB = HB;
+    thrust::device_vector<int> DMUL = HB;
+    thrust::device_vector<int> DMOD = HB;
 
+    // Perform operations
     for (int i = 0; i < DA.size(); i++) {
-        DC[i] = DA[i] + DB[i];
+        DADD[i] = DA[i] + DB[i];
+        DSUB[i] = DA[i] - DB[i];
+        DMUL[i] = DA[i] * DB[i];
+        DMOD[i] = DA[i] % DB[i];
     }
 
-    // print contents of D
-    for(int i = 0; i < D.size(); i++) {
-        std::cout << "DC[" << i << "] = " << DC[i] << std::endl;
+    // print addition results
+    printf("\nResult of addition: \n");
+    for(int i = 0; i < DA.size(); i++) {
+        std::cout << "DC[" << i << "] = " << DADD[i] << std::endl;
+    }
+
+    // print subtraction results
+    printf("\nResult of subtraction: \n");
+    for(int i = 0; i < DA.size(); i++) {
+        std::cout << "DC[" << i << "] = " << DSUB[i] << std::endl;
+    }
+
+    // print multiplication results
+    printf("\nResult of multiplication: \n");
+    for(int i = 0; i < DA.size(); i++) {
+        std::cout << "DC[" << i << "] = " << DMUL[i] << std::endl;
+    }
+
+    // print modulo results
+    printf("\nResult of modulus: \n");
+    for(int i = 0; i < DA.size(); i++) {
+        std::cout << "DC[" << i << "] = " << DMOD[i] << std::endl;
     }
 
 
