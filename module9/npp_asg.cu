54669fa8923004d3457d4ca2830b5a96c0357ca4
theodoredyer <tdyer@ucsc.edu>
2022-04-13 18:08:35 -0700

    add test pgm

---
+++ module9/npp_asg.cu
@@ -0,0 +1,185 @@
+/*
+Problem Spec: 
+- Create a program that utilizes the NPP library. 
+- Executes kernel using NPP library to execute simple operations
+- Test harness executes two separate runs of each kernel
+
+Theodore Dyer
+Introduction to GPU Programming Spring 2022 (EN605.617.81)
+Chance Pascale
+4/13/2022
+*/
+
+// Note - following the provided boxfilter example file as reference. 
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#  define WINDOWS_LEAN_AND_MEAN
+#  define NOMINMAX
+#  include <windows.h>
+#  pragma warning(disable:4819)
+#endif
+
+#include <ImagesCPU.h>
+#include <ImagesNPP.h>
+#include <ImageIO.h>
+#include <Exceptions.h>
+
+#include <string.h>
+#include <fstream>
+#include <iostream>
+
+#include <cuda_runtime.h>
+#include <npp.h>
+
+#include <helper_string.h>
+#include <helper_cuda.h>
+
+inline int cudaDeviceInit(int argc, const char **argv)
+{
+    int deviceCount;
+    checkCudaErrors(cudaGetDeviceCount(&deviceCount));
+
+    if (deviceCount == 0)
+    {
+        std::cerr << "CUDA error: no devices supporting CUDA." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int dev = findCudaDevice(argc, argv);
+
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, dev);
+    std::cerr << "cudaSetDevice GPU" << dev << " = " << deviceProp.name << std::endl;
+
+    checkCudaErrors(cudaSetDevice(dev));
+
+    return dev;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("%s Starting boxfilter conversion...\n\n", argv[0]);
+
+    try
+    {
+        std::string sFilename;
+        char *filePath;
+
+        cudaDeviceInit(argc, (const char **)argv);
+
+        if (checkCmdLineFlag(argc, (const char **)argv, "input"))
+        {
+            getCmdLineArgumentString(argc, (const char **)argv, "input", &filePath);
+        }
+        else
+        {
+            filePath = sdkFindFilePath("ghost.pgm", argv[0]);
+        }
+
+        if (filePath)
+        {
+            sFilename = filePath;
+        }
+        else
+        {
+            sFilename = "ghost.pgm";
+        }
+
+        // if we specify the filename at the command line, then we only test sFilename[0].
+        int file_errors = 0;
+        std::ifstream infile(sFilename.data(), std::ifstream::in);
+
+        if (infile.good())
+        {
+            std::cout << "boxFilterNPP opened: <" << sFilename.data() << "> successfully!" << std::endl;
+            file_errors = 0;
+            infile.close();
+        }
+        else
+        {
+            std::cout << "boxFilterNPP unable to open: <" << sFilename.data() << ">" << std::endl;
+            file_errors++;
+            infile.close();
+        }
+
+        if (file_errors > 0)
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        std::string sResultFilename = sFilename;
+
+        std::string::size_type dot = sResultFilename.rfind('.');
+
+        if (dot != std::string::npos)
+        {
+            sResultFilename = sResultFilename.substr(0, dot);
+        }
+
+        sResultFilename += "_boxFilter.pgm";
+
+        if (checkCmdLineFlag(argc, (const char **)argv, "output"))
+        {
+            char *outputFilePath;
+            getCmdLineArgumentString(argc, (const char **)argv, "output", &outputFilePath);
+            sResultFilename = outputFilePath;
+        }
+
+        // declare a host image object for an 8-bit grayscale image
+        npp::ImageCPU_8u_C1 oHostSrc;
+        // load gray-scale image from disk
+        npp::loadImage(sFilename, oHostSrc);
+        // declare a device image and copy construct from the host image,
+        // i.e. upload host to device
+        npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
+
+        // create struct with box-filter mask size
+        NppiSize oMaskSize = {5, 5};
+
+        NppiSize oSrcSize = {(int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
+        NppiPoint oSrcOffset = {0, 0};
+
+        // create struct with ROI size
+        NppiSize oSizeROI = {(int)oDeviceSrc.width() , (int)oDeviceSrc.height() };
+        // allocate device image of appropriately reduced size
+        npp::ImageNPP_8u_C1 oDeviceDst(oSizeROI.width, oSizeROI.height);
+        // set anchor point inside the mask to (oMaskSize.width / 2, oMaskSize.height / 2)
+        // It should round down when odd
+        NppiPoint oAnchor = {oMaskSize.width / 2, oMaskSize.height / 2};
+
+        // run box filter
+        NPP_CHECK_NPP (
+                           nppiFilterBoxBorder_8u_C1R(oDeviceSrc.data(), oDeviceSrc.pitch(),
+                                                      oSrcSize, oSrcOffset,
+                                                      oDeviceDst.data(), oDeviceDst.pitch(),
+                                                      oSizeROI, oMaskSize, oAnchor, NPP_BORDER_REPLICATE) );
+
+        // declare a host image for the result
+        npp::ImageCPU_8u_C1 oHostDst(oDeviceDst.size());
+        // and copy the device result data into it
+        oDeviceDst.copyTo(oHostDst.data(), oHostDst.pitch());
+
+        saveImage(sResultFilename, oHostDst);
+        std::cout << "Saved image: " << sResultFilename << std::endl;
+
+        exit(EXIT_SUCCESS);
+    }
+    catch (npp::Exception &rException)
+    {
+        std::cerr << "Program error! The following exception occurred: \n";
+        std::cerr << rException << std::endl;
+        std::cerr << "Aborting." << std::endl;
+
+        exit(EXIT_FAILURE);
+    }
+    catch (...)
+    {
+        std::cerr << "Program error! An unknow type of exception occurred. \n";
+        std::cerr << "Aborting." << std::endl;
+
+        exit(EXIT_FAILURE);
+        return -1;
+    }
+
+    return 0;
+}
